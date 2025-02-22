43c384e955313892c391a68ee0c22853a491baf8
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-02-22 22:09:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function convertToRoman(num) {
-  	const romanSymbols = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+    // Define the Roman numeral symbols and their corresponding values
+    const romanSymbols = [
+        ['M', 1000],
+        ['D', 500],
+        ['C', 100],
+        ['L', 50],
+        ['X', 10],
+        ['V', 5],
+        ['I', 1]
+    ];
 
-  //your code here
-	  let result = '';
+    let result = '';
 
     // Iterate over the symbols and build the Roman numeral
     for (let [symbol, value] of romanSymbols) {
@@ -22,8 +22,6 @@ function convertToRoman(num) {
     }
 
     return result; // Return the final Roman numeral
-
-
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
