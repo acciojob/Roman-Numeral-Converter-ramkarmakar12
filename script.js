5efc870e2e98f603c7745488bfa8c2d66f2079cf
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-02-22 22:02:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ function convertToRoman(num) {
     };
 
   //your code here
+	  let result = '';
+
+    // Iterate over the symbols and build the Roman numeral
+    for (let [symbol, value] of romanSymbols) {
+        // While the number is greater than or equal to the value
+        while (num >= value) {
+            result += symbol; // Append the symbol to the result
+            num -= value;     // Decrease the number by the value
+        }
+    }
+
+    return result; // Return the final Roman numeral
+
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
